f196092453503e459782190ccb1a064fda0d700e
Mason S Russell <masonsrussell@outlook.com>
2024-09-26 18:23:39 -0400

    Removes Resources Section

--- src/lib/utils/tabs.js
+++ src/lib/utils/tabs.js
@@ -77,6 +77,7 @@ export const tabs = [
                 dest: `https://sleeper.app/leagues/${leagueID}`,
             },
         ]
+<<<<<<< HEAD
     },
     {
         icon: 'lightbulb',
@@ -84,4 +85,7 @@ export const tabs = [
         dest: '/resources',
         key: 'resources',
     },
+=======
+    }
+>>>>>>> 0b4179f (Removes Resources Section)
 ];
