ac741992aa83a0a15f5535c61ea5682a8dd86e06
Mason S Russell <masonsrussell@outlook.com>
2025-08-06 22:31:08 -0400

    Revert "Adds 2025 season ID"
    This reverts commit d44fe504c87022c1a02396d1a0bf3d1f4f0d012b.

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "1180600975043792896"; // your league ID
+export const leagueID = "1048281947404955648"; // your league ID
 export const leagueName = "Show Us Your TD's"; // your league name
 export const dues = 100; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
