1f42b29b5f637ac555133a03f0b5a2a4924432d5
Mason S Russell <masonsrussell@outlook.com>
2025-08-06 22:00:50 -0400

    More merge conflicts

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -162,28 +162,7 @@
         <li>K</li>
     </ul>
     
-<<<<<<< HEAD
-    <p>18 Bench (Expanded by 5 between the keague draft and start of regular season)</p>
-
-    <p>1 IR Spots (Player must be labeled as IR, Out, or Covid in the Sleeper App)</p>
-    
-        <h4 bind:this={oneOneOne}>1.1.1 Position Maximums</h4>
-        
-        <div class="subBlock">
-            <table class="positionMaximums">
-                <tbody>
-                <tr><td>QB</td><td class="right">3 active, 5 total</td></tr>
-                <tr><td>RB</td><td class="right">20</td></tr>
-                <tr><td>WR</td><td class="right">20</td></tr>
-                <tr><td>TE</td><td class="right">10</td></tr>
-                <tr><td>D/ST</td><td class="right">3</td></tr>
-                <tr><td>K</td><td class="right">3</td></tr>
-                </tbody>
-            </table>
-        </div>
-=======
     <p>5 Bench</p>
->>>>>>> 35eb99b (Updates the Constitution page)
 
     <p>2 IR Spots (Player must be labeled as IR or PUP in the Sleeper App)</p>
 
